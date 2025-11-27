a429788a70218c9bd508bf9f75f9a63697e23484
Yudi Omaki <yudi.omaki@sou.inteli.edu.br>
2025-11-27 14:25:39 -0300

    Update WiFi and MQTT configuration details

--- script.cpp
+++ script.cpp
@@ -2,17 +2,14 @@
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 
+const char* ssid = "SEU_WIFI"; 
+const char* password = "SUA_SENHA_WIFI";
 
-const char* ssid = "Inteli.Iot"; 
-const char* password = "%(Yk(sxGMtvFEs.3";
+const char* mqtt_server = "SEU_URL_HIVEMQ.s1.eu.hivemq.cloud"; 
+const int mqtt_port = 8883; 
+const char* mqtt_user = "SEU_USUARIO_HIVEMQ"; 
+const char* mqtt_password = "SUA_SENHA_HIVEMQ";
 
-// CONFIGURAÇÕES DO HIVEMQ CLOUD 
-const char* mqtt_server = "ce8024cdd8084f91b9f0e896819b210b.s1.eu.hivemq.cloud"; 
-const int mqtt_port = 8883; // Porta segura (SSL)
-const char* mqtt_user = "hivemq.webclient.1764252624556"; // Usuário que você criou na aba 'Access Management'
-const char* mqtt_password = "4&rl3%a?iMRx5SO,yYJ6";
-
-// Tópico onde publicaremos os dados
 const char* topic_publish = "inteli/projeto/rssi";
 
 WiFiClientSecure espClient;
@@ -74,22 +71,16 @@ void loop() {
   client.loop();
 
   unsigned long now = millis();
-  // Publica a cada 1 segundo (1000ms)
   if (now - lastMsg > 1000) {
     lastMsg = now;
     
-    // 1. Ler a potência do sinal (RSSI)
     long rssi = WiFi.RSSI();
 
-    // 2. Imprimir na Serial (como pedido no enunciado)
     Serial.print("Sinal WiFi (dBm): ");
     Serial.println(rssi);
 
-    // 3. Publicar no HiveMQ
-    // Converter o número para string
     snprintf (msg, MSG_BUFFER_SIZE, "%ld", rssi);
     
-    // Publica no tópico
     client.publish(topic_publish, msg);
   }
 }
